bfe987998287b628c6a645c0ca2a5406bad30bf0
Sukhbinder Singh <sukhbindersingh@gmail.com>
2025-03-14 21:06:55 +0530

    Black formatting

--- tests/test_vidtoolz_add_text.py
+++ tests/test_vidtoolz_add_text.py
@@ -5,13 +5,16 @@ from argparse import ArgumentParser
 from vidtoolz_add_text.add_text import add_text_to_video, write_file
 import os
 
+
 def test_create_parser():
     subparser = ArgumentParser().add_subparsers()
     parser = w.create_parser(subparser)
 
     assert parser is not None
 
-    result = parser.parse_args(['video.mp4', 'hello', "-st", "3", "-et", "7", "-f", "100"])
+    result = parser.parse_args(
+        ["video.mp4", "hello", "-st", "3", "-et", "7", "-f", "100"]
+    )
     assert result.main_video == "video.mp4"
     assert result.text == "hello"
     assert result.start_time == 3
@@ -29,76 +32,103 @@ def test_plugin(capsys):
 
 HERE = os.path.dirname(__file__)
 # Define a test video file path
-TEST_VIDEO_FILE = os.path.join(HERE, 'test_video.mp4')
+TEST_VIDEO_FILE = os.path.join(HERE, "test_video.mp4")
 
 # Define a temporary output video file path
-TEMP_OUTPUT_VIDEO_FILE = os.path.join(HERE, 'temp_output.mp4')
+TEMP_OUTPUT_VIDEO_FILE = os.path.join(HERE, "temp_output.mp4")
+
 
 def test_add_text_to_video():
     # Test adding text to a video
-    video_with_text, fps = add_text_to_video(TEST_VIDEO_FILE, 'Test text', 0, 5, 'center')
+    video_with_text, fps = add_text_to_video(
+        TEST_VIDEO_FILE, "Test text", 0, 5, "center"
+    )
     assert video_with_text is not None
     assert fps > 0
+
 
 def test_add_text_to_video_invalid_position():
     # Test adding text to a video with an invalid position
-    video_with_text, fps = add_text_to_video(TEST_VIDEO_FILE, 'Test text', 0, 5, 'invalid-position')
+    video_with_text, fps = add_text_to_video(
+        TEST_VIDEO_FILE, "Test text", 0, 5, "invalid-position"
+    )
     assert video_with_text is not None
     assert fps > 0
 
+
 def test_write_file():
     # Test writing the video to a file
-    video_with_text, fps = add_text_to_video(TEST_VIDEO_FILE, 'Test text', 2, 5, 'center')
+    video_with_text, fps = add_text_to_video(
+        TEST_VIDEO_FILE, "Test text", 2, 5, "center"
+    )
     write_file(video_with_text, TEMP_OUTPUT_VIDEO_FILE, fps)
     assert os.path.exists(TEMP_OUTPUT_VIDEO_FILE)
 
+
 def test_write_file_invalid_output_path():
     # Test writing the video to an invalid output path
-    video_with_text, fps = add_text_to_video(TEST_VIDEO_FILE, 'Test text', 0, 5, 'center')
+    video_with_text, fps = add_text_to_video(
+        TEST_VIDEO_FILE, "Test text", 0, 5, "center"
+    )
     with pytest.raises(SystemExit):
-        write_file(video_with_text, '/invalid/output/path', fps)
+        write_file(video_with_text, "/invalid/output/path", fps)
+
 
 def test_add_text_to_video_invalid_video_file():
     # Test adding text to an invalid video file
     with pytest.raises(SystemExit):
-        add_text_to_video('invalid_video_file.mp4', 'Test text', 0, 5, 'center')
+        add_text_to_video("invalid_video_file.mp4", "Test text", 0, 5, "center")
+
 
 def test_add_text_to_video_invalid_text():
     # Test adding invalid text to a video
     with pytest.raises(SystemExit):
-        add_text_to_video(TEST_VIDEO_FILE, None, 0, 5, 'center')
+        add_text_to_video(TEST_VIDEO_FILE, None, 0, 5, "center")
+
 
 def test_add_text_to_video_invalid_fontsize():
     # Test adding text to a video with an invalid font size
     with pytest.raises(SystemExit):
-        add_text_to_video(TEST_VIDEO_FILE, 'Test text', 0, 5, 'center', fontsize='invalid')
+        add_text_to_video(
+            TEST_VIDEO_FILE, "Test text", 0, 5, "center", fontsize="invalid"
+        )
+
 
 def test_add_text_to_video_invalid_start_time():
     # Test adding text to a video with an invalid start time
     with pytest.raises(SystemExit):
-        add_text_to_video(TEST_VIDEO_FILE, 'Test text', 'invalid', 5, 'center')
+        add_text_to_video(TEST_VIDEO_FILE, "Test text", "invalid", 5, "center")
+
 
 def test_add_text_to_video_invalid_end_time():
     # Test adding text to a video with an invalid end time
     with pytest.raises(SystemExit):
-        add_text_to_video(TEST_VIDEO_FILE, 'Test text', 0, 'invalid', 'center')
+        add_text_to_video(TEST_VIDEO_FILE, "Test text", 0, "invalid", "center")
+
 
 def test_write_file_invalid_video_with_text():
     # Test writing an invalid video to a file
     with pytest.raises(SystemExit):
         write_file(None, TEMP_OUTPUT_VIDEO_FILE, 30)
 
+
 def test_write_file_invalid_output_video_path():
     # Test writing a video to an invalid output path
-    video_with_text, fps = add_text_to_video(TEST_VIDEO_FILE, 'Test text', 0, 5, 'center')
+    video_with_text, fps = add_text_to_video(
+        TEST_VIDEO_FILE, "Test text", 0, 5, "center"
+    )
     with pytest.raises(SystemExit):
-        write_file(video_with_text, '/invalid/output/path', fps)
+        write_file(video_with_text, "/invalid/output/path", fps)
+
 
 def test_write_file_invalid_fps():
     # Test writing a video with an invalid FPS to a file
-    video_with_text, fps = add_text_to_video(TEST_VIDEO_FILE, 'Test text', 0, 5, 'center')
+    video_with_text, fps = add_text_to_video(
+        TEST_VIDEO_FILE, "Test text", 0, 5, "center"
+    )
     with pytest.raises(SystemExit):
-        write_file(video_with_text, TEMP_OUTPUT_VIDEO_FILE, 'invalid')
+        write_file(video_with_text, TEMP_OUTPUT_VIDEO_FILE, "invalid")
+
 
 def teardown_module():
     # Remove the temporary output video file
--- vidtoolz_add_text/add_text.py
+++ vidtoolz_add_text/add_text.py
@@ -11,13 +11,15 @@ POSITION_MAP = {
 }
 
 
-def add_text_to_video(input_video_path, text, start_time, end_time, position, fontsize=50):
+def add_text_to_video(
+    input_video_path, text, start_time, end_time, position, fontsize=50
+):
     try:
         # Load the original video
         video = VideoFileClip(input_video_path)
     except Exception as e:
         sys.exit("Error loading video file: " + str(e))
-        
+
     try:
         # Create a text clip using a common system font.
         txt_clip = TextClip(
@@ -29,36 +31,44 @@ def add_text_to_video(input_video_path, text, start_time, end_time, position, fo
             # method="caption",
             stroke_width=2,
             stroke_color="black",
-            color="white",)
-            #text, fontsize=fontsize, color='white', font='Arial')
+            color="white",
+        )
+        # text, fontsize=fontsize, color='white', font='Arial')
     except Exception as e:
         sys.exit("Error creating text clip: " + str(e))
-    
+
     # Convert position string to a tuple
     pos_tuple = POSITION_MAP.get(position, ("center", "bottom"))
     try:
         # Set duration and starting time, and position the text
-        txt_clip = (txt_clip
-                    .with_position(pos_tuple)
-                    .with_duration(end_time - start_time)
-                    .with_start(start_time))
+        txt_clip = (
+            txt_clip.with_position(pos_tuple)
+            .with_duration(end_time - start_time)
+            .with_start(start_time)
+        )
     except Exception as e:
         sys.exit("Error setting properties on text clip: " + str(e))
-    
+
     try:
         # Overlay the text clip on the video
         video_with_text = CompositeVideoClip([video, txt_clip])
     except Exception as e:
         sys.exit("Error combining clips: " + str(e))
-    
+
     return video_with_text, video.fps
 
 
 def write_file(video_with_text, output_video_path, fps):
     try:
         # Write the result to a file
-        video_with_text.write_videofile(output_video_path, codec='libx264', fps=fps, audio_codec="aac",temp_audiofile='temp_audio.m4a',
-        remove_temp=True,)
+        video_with_text.write_videofile(
+            output_video_path,
+            codec="libx264",
+            fps=fps,
+            audio_codec="aac",
+            temp_audiofile="temp_audio.m4a",
+            remove_temp=True,
+        )
     except Exception as e:
         sys.exit("Error writing video file: " + str(e))
     video_with_text.close()
