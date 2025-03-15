b6d3a1df50246aa14bc63f8b086b6fb19ac30268
Sukhbinder Singh <sukhbindersingh@gmail.com>
2025-03-15 08:20:42 +0530

    TTF font

--- vidtoolz_add_text/add_text.py
+++ vidtoolz_add_text/add_text.py
@@ -21,7 +21,7 @@ def add_text_to_video(
         sys.exit("Error loading video file: " + str(e))
 
     try:
-        font = "/fonts/LucidaGrande.ttc"
+        font = "/fonts/Raleway-Bold.ttf"
         # Create a text clip using a common system font.
         txt_clip = TextClip(
             font=font,
